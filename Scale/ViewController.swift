b31cc5825fdf3a8ac5ac435ce14b8aa32c1f8f5a
Sahil <sahil.va.us@gmail.com>
2022-07-13 09:56:48 -0400

    inital commit

--- Scale/ViewController.swift
+++ Scale/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if let touch = touches.first
+        {
+            if #available(iOS 9.0, *)
+            {
+                if traitCollection.forceTouchCapability == UIForceTouchCapability.available
+                {
+                    if touch.force >= touch.maximumPossibleForce{
+                        forceLabel.text = "385+ grams"
+                    }
+                    else
+                    {
+                        let force = touch.force/touch.maximumPossibleForce
+                        let grams = force * 365
+                        let roundGrams = Int(grams)
+                        forceLabel.text = "\(roundGrams) grams"
+                    }
+                }
+            }
+        }
+        
+    }
 }
 
